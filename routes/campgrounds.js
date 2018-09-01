163d429e97737ed8ca8f62711da18c83fa861f8f
Abel Sobmenso <abelsobmenso@gmail.com>
2018-09-01 18:35:31 +0000

    modify price

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -60,7 +60,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var lat = data[0].latitude;
     var lng = data[0].longitude;
     var location = data[0].formattedAddress;
-    var newCampground = {name: name, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
+    var newCampground = {name: name, price: price, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
